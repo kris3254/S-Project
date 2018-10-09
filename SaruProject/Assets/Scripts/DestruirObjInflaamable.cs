0756b60f6bfe60503fcb738ab19c436ca914943c
LauraPalomo <lpalomovelasco@hotmail.com>
2018-10-09 21:24:13 +0200

    Inflamables3
    Intento de destruir el objeto

--- SaruProject/Assets/Scripts/DestruirObjInflaamable.cs
+++ SaruProject/Assets/Scripts/DestruirObjInflaamable.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestruirObjInflaamable : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+public class DestruirObjInflaamable : MonoBehaviour 
+{
+	GameObject cosa;
+	public void Start ()
+	{
+		cosa = GameObject.Find ("inflamable");
+		Destroy (cosa,(float) 1);
+		Destroy (this.gameObject,(float)1);
 	}
 }
