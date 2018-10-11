fcbd7ce82cbaf16f08747963ea7592b59e356716
DelonK <delon_asd@hotmail.com>
2018-10-11 19:59:11 +0200

    Cámaras 0.5 (me pisaron dos cambios wey)

---
+++ SaruProject/Assets/Scripts/CinemachineTimelinePlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class CinemachineTimelinePlay : MonoBehaviour {
+
+    public PlayableDirector playableDirector;
+    private bool isFirstTime;
+
+    private void Start()
+    {
+        isFirstTime = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.gameObject.tag == "Player") && (isFirstTime == true))
+        {
+            Debug.Log("Saru Entra Puente");
+            playableDirector.enabled = true;
+            isFirstTime = false;
+        }
+    }
+
+}
