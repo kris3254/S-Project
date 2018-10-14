38635514ef8b9efe023e6f82f406ad0b97af0fac
Rafatypicat <Rafatypicat@users.noreply.github.com>
2018-10-14 13:47:26 +0200

    Partículas
    Partículas efecto sword (para el cayado) y humo de pasos (para Saru) localizadas y adjuntadas en la jerarquía
    
    Proximos pasos: Animaciones en las plataformas ocultas y en los mini-orbes (jugar con el emmisive, que reluzcan y que se apagen)

---
+++ SaruProject/Assets/Animations/Saru/Iddle, Jump, Roll and Run/Activarparticulas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activarparticulas: MonoBehaviour {
+
+	public ParticleSystem PieDerecho;
+	public ParticleSystem PieIzquierdo;
+
+	public void ActivarPieDerecho ()
+	{
+		PieDerecho.Play();
+	}
+
+	public void ActivarPieIzquierdo ()
+	{
+		PieIzquierdo.Play ();
+	}
+}		
