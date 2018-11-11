844d08bae3430f0f233763e18fc70eb28143151d
DelonK <delon_asd@hotmail.com>
2018-11-11 21:37:09 +0100

    Cinemática Inicial

--- SaruProject/Assets/Scripts/PlayCinematic.cs
+++ SaruProject/Assets/Scripts/PlayCinematic.cs
@@ -20,28 +20,36 @@ public class PlayCinematic : MonoBehaviour {
     private VideoPlayer vp;
     private Camera cinematicCamera;
     public VideoClip cinematicVideoClip;
+    public VideoClip startingVideoClip;
 
     public Camera mainCamera;
 
     public Transform respawnTransform;
-	public playerControllerCustom  pController;
+    public playerControllerCustom pController;
+
+    public bool isStartingCinematic = false;
 
     private void Awake()
     {
         vp = cameraForPlayingCinematics.GetComponent<VideoPlayer>();
         isFirstTime = true;
         cinematicCamera = cameraForPlayingCinematics.GetComponent<Camera>();
-        cameraForPlayingCinematics.SetActive(false);
+    }
+
+    private void Start()
+    {
+        if (isStartingCinematic == true)
+        StartCoroutine("StartingCinematic");
     }
 
 
     void EndReached(VideoPlayer videoplayer)
     {
         videoplayer.Stop();
-        
+
         //justo antes de volver a la camara de gameplay muevo al player a la posicion donde finaliza la cinemática
         //solo en el caso de las cinematicas donde suceda esto (la posicion de respawn es !=null)
-        if(respawnTransform!=null)
+        if (respawnTransform != null)
         {
             PlayerManager.instance.gameObject.SetActive(false);
             PlayerManager.instance.gameObject.transform.position = respawnTransform.position;
@@ -54,12 +62,12 @@ public class PlayCinematic : MonoBehaviour {
         cameraForPlayingCinematics.SetActive(false);
         mainCamera.depth = 0;
         vp.loopPointReached -= EndReached;
-		pController.canDoThings = true;
+        pController.canDoThings = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if((other.gameObject.tag=="Player") && (isFirstTime))
+        if ((other.gameObject.tag == "Player") && (isFirstTime))
         {
             vp.loopPointReached += EndReached;
             vp.clip = cinematicVideoClip;
@@ -71,12 +79,29 @@ public class PlayCinematic : MonoBehaviour {
             cameraForPlayingCinematics.SetActive(true);
             isFirstTime = false;
 
-			pController.canDoThings = false;
+            pController.canDoThings = false;
             pController.anim.SetFloat("speed", 0f);
 
         }
 
     }
+
+    IEnumerator StartingCinematic()
+    {
+        vp.loopPointReached += EndReached;
+        vp.clip = startingVideoClip;
+        vp.Play();
+        UIManager.instance.HideHud();
+        LevelManager.instance.LockPauseMenu();
+        mainCamera.depth = -1;
+        cinematicCamera.depth = 0;
+        cameraForPlayingCinematics.SetActive(true);
+        pController.canDoThings = false;
+        pController.anim.SetFloat("speed", 0f);
+        yield return new WaitForSeconds(0);
+    }
+    
+
 
 
 }
