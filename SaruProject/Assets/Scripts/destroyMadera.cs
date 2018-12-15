bd04758ce3c8676b0ccdef06ebad4ee1a99019e9
DelonK <delon_asd@hotmail.com>
2018-12-15 21:20:18 +0100

    Bolitas con Madera Destruibe

---
+++ SaruProject/Assets/Scripts/destroyMadera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyMadera : MonoBehaviour {
+
+    public GameObject destroyedVersion;
+
+    private Component[] trocitosRB;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            GameObject newMadera = Instantiate(destroyedVersion, transform.position, transform.rotation) as GameObject;
+            Destroy(GetComponent<Transform>().GetChild(0).gameObject);
+
+            trocitosRB = newMadera.GetComponentsInChildren<Rigidbody>();
+
+            foreach (Rigidbody rb in trocitosRB)
+                rb.AddForce(Random.Range(0,3), Random.Range(0,3), Random.Range(0,3), ForceMode.Impulse);
+            // newMadera.GetComponentInChildren<Rigidbody>().AddForce(0, 3, 3, ForceMode.Impulse);
+
+            Destroy(newMadera, 5f);
+        }
+    }
+}
