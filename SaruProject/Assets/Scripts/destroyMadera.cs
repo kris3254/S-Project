381fce50d4445be2f047858bcedb91d07aaa58f6
DelonK <delon_asd@hotmail.com>
2018-12-17 20:27:19 +0100

    Bolita Madera Animada Beta

--- SaruProject/Assets/Scripts/destroyMadera.cs
+++ SaruProject/Assets/Scripts/destroyMadera.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class destroyMadera : MonoBehaviour {
 
     public GameObject[] destroyedVersion;
+    public GameObject oldMadera;
 
     private Component[] trocitosRB;
+    private Component[] trocitosCollider;
+    private bool _maderaDestroyed;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
         {
             GameObject newMadera = Instantiate(destroyedVersion[Random.Range(0,2)], transform.position, transform.rotation) as GameObject;
-            if (GetComponent<Transform>().childCount != 0)
-                Destroy(GetComponent<Transform>().GetChild(0).gameObject);
+            // He probado con la position de todos los hijos de Woodball Enemy, la localPosition, buscando la diferencia de alturas... y no hay manera :>!
+
+            trocitosCollider = newMadera.GetComponentsInChildren<Collider>();
+
+            foreach (Collider collider in trocitosCollider)
+                Physics.IgnoreCollision(collider, GetComponent<Collider>());
+
+            if (!_maderaDestroyed)
+            {
+                Destroy(oldMadera);
+                _maderaDestroyed = true;
+            }
 
             trocitosRB = newMadera.GetComponentsInChildren<Rigidbody>();
 
             foreach (Rigidbody rb in trocitosRB)
                 rb.AddForce(Random.Range(0,3), Random.Range(0,3), Random.Range(0,3), ForceMode.Impulse);
-            // newMadera.GetComponentInChildren<Rigidbody>().AddForce(0, 3, 3, ForceMode.Impulse);
 
             Destroy(newMadera, 5f);
         }
     }
+
 }
